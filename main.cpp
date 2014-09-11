3118c702213c7220b273ed5e2842a36d6cee40c3
Alex Burn <aburnoffice@gmail.com>
2014-09-11 11:35:29 +0100

    added licence boilerplate

--- main.cpp
+++ main.cpp
@@ -7,6 +7,28 @@
 #include <algorithm>
 #include <fstream>
 
+/* ------------------------------------------------------------------------
+ *
+ *		© Copyright 2014 Alex Burn
+ *      Contact: aburnoffice@gmail.com
+ *
+ *      This file is part of fallout-hacking-minigame.
+ *
+ *      fallout-hacking-minigame is free software: you can redistribute it and/or modify
+ *      it under the terms of the GNU General Public License as published by
+ *      the Free Software Foundation, either version 3 of the License, or
+ *      (at your option) any later version.
+ *
+ *      fallout-hacking-minigame is distributed in the hope that it will be useful,
+ *      but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *      MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *      GNU General Public License for more details.
+ *
+ *      You should have received a copy of the GNU General Public License
+ *      along with fallout-hacking-minigame.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ ------------------------------------------------------------------------ */
+
 std::vector<std::string> open_read_from_file(const char* input_file, char delimiter)
 {
     std::vector<std::string> parsed_data;
